60e53a291a05fd880c63d6f52b48685ef7c7f0b6
Klangfabrik <98235604+Klangfabrik@users.noreply.github.com>
2025-02-05 11:40:40 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+let lautstärke = 0
+basic.showString("hi!")
+basic.forever(function () {
+    lautstärke = input.soundLevel()
+    if (lautstärke < 90) {
+        basic.setLedColors(0x00ff00, 0x000000, 0x000000)
+    } else if (lautstärke < 180) {
+        basic.setLedColors(0x00ff00, 0xff8000, 0x000000)
+    } else if (lautstärke > 180) {
+        basic.setLedColors(0x00ff00, 0xff8000, 0xff0000)
+    } else {
+        basic.showString("Fehler")
+    }
+})
