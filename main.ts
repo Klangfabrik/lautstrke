555f43e4994abf1863a83eb84077655a0da5baf8
Klangfabrik <98235604+Klangfabrik@users.noreply.github.com>
2025-02-05 11:42:16 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ let lautstärke = 0
 basic.showString("hi!")
 basic.forever(function () {
     lautstärke = input.soundLevel()
-    if (lautstärke < 90) {
+    if (lautstärke < 50) {
         basic.setLedColors(0x00ff00, 0x000000, 0x000000)
-    } else if (lautstärke < 180) {
+    } else if (lautstärke < 90) {
         basic.setLedColors(0x00ff00, 0xff8000, 0x000000)
-    } else if (lautstärke > 180) {
+    } else if (lautstärke > 90) {
         basic.setLedColors(0x00ff00, 0xff8000, 0xff0000)
     } else {
         basic.showString("Fehler")
